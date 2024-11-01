7549e2ccf5150d8f397748a8981a437bc16382fa
Johan <jmpareds@gmail.com>
2024-10-31 23:26:42 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <winsock.h>
+using namespace std;
+
+int main (){
+    // Initialize the socket
+
+    // Initialize the environment for sockaddr structure
+
+    // Bind the socket to the local port
+
+    // Listen the request from the client (queues the requests)
+
+    // Keep waiting for new requests and proceed as per the request
+    
+}
