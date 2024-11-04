b47ac44f1a6d3b39e74b1aec13a6c20cbc92b842
Johan <jmpareds@gmail.com>
2024-11-03 22:42:48 -0400

    Now game is synchronized between the two sockets

--- public/client.js
+++ public/client.js
@@ -1,11 +1,13 @@
 
 //Establishing a connection with the server
 const socket = io();
+let game_id;
+let username;
 
 document.getElementById("play--container").style.display = "none";
 document.getElementById("waiting--container").style.display = "none";
 document.getElementById("submit-name").addEventListener('click', () => {
-    let username = document.getElementById("name").value;
+    username = document.getElementById("name").value;
     socket.emit('newPlayer', username);
 
     document.getElementById("enter-name").style.display = "none";
@@ -20,7 +22,58 @@ socket.on('startGame', (game) => {
     const p2 = document.querySelector('.p2--id');
     p1.innerHTML = game.p1_id;
     p2.innerHTML = game.p2_id;
+
+    document.getElementById("game--status").innerHTML = game.currentPlayer + " turn";
+    console.log(game.game_id + " has started");
+    game_id = game.game_id;
 });
+
+let cells = document.getElementsByClassName("cell");
+function handleCellClick(cellIndex) {
+    socket.emit('playerMove', game_id, cellIndex);
+}
+
+let clickHandlers = [];
+
+function createCellClickListener(cellIndex) {
+    return function() {
+      handleCellClick(cellIndex);
+    };
+}
+
+for (let i = 0; i < cells.length; i++) {
+    const clickHandler = createCellClickListener(i);
+    clickHandlers.push(clickHandler);
+    cells[i].addEventListener("click", clickHandlers[i]);
+}
+
+socket.on('updateGame', (game) => {
+    console.log(JSON.stringify(game));
+    document.getElementById("game--status").innerHTML = game.currentPlayer + " turn";
+
+    for(let i = 0; i < 9; i++) {
+        document.getElementsByClassName('game--container')[0].children[i].innerHTML = game.state[i];
+    }
+});
+
+socket.on('gameOver', (game) => { 
+    if (game.winner === null) {
+        document.getElementById("game--status").innerHTML = "Draw!";
+    } else {
+        document.getElementById("game--status").innerHTML = game.winner + " wins!";
+    }
+
+    document.getElementsByClassName("new-game--button").innerHTML = "New game";
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener("click", clickHandlers[i]);
+    }
+});
+
+document.getElementById('new-game--button').addEventListener("click", () => {
+    socket.emit('newGame');
+});
+
+
 
 // //Client sends a message at the moment it got connected with the server
 // socket.emit('clientToServer', "Hello, server!");
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 3000
 const server = app.listen(port)
 const io = require('socket.io')(server)
 const path = require('path');
+const { randomBytes } = require('node:crypto');
+const { arch } = require('os')
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,24 +14,65 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 class Game {
     state;
     p1_id;
     p2_id;
     currentPlayer;
-    gameActive = true;
+    gameActive;
+    game_id;
+    winner;
 
     constructor(p1_id, p2_id){
        this.state = ["", "", "", "", "", "", "", "", ""];
        this.p1_id = p1_id;
        this.p2_id = p2_id;
-       this.currentPlayer = this.player1;
+       this.currentPlayer = 'X';
        this.gameActive = true;
+       this.game_id = randomBytes(16).toString("hex");
+    }
+
+    checkGameOver(){
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            let a = this.state[winCondition[0]];
+            let b = this.state[winCondition[1]];
+            let c = this.state[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                this.gameActive = false;
+                this.winner = (this.currentPlayer === 'X') ? this.p1_id : this.p2_id;
+                return true;
+            }
+        }
+
+        // check for a draw
+        if (!this.state.includes('')) {
+            this.winner = null;
+            this.gameActive = false;
+            return true;
+        }
+
+        return false;
     }
 }
 
 let players = [];
+let archived_games = [];
+let active_games = [];
 
 //The 'connection' is a reserved event name in socket.io
 //For whenever a connection is established between the server and a client
@@ -40,9 +83,29 @@ io.on('connection', (socket) => {
 
             if (players.length == 2){
                 const game = new Game(players[0].username, players[1].username);
+                active_games.push(game);
                 io.emit('startGame', game);
             }
-    }); 
+    });
+    
+    socket.on('playerMove', (game_id, cell_index) => {
+        console.log(`game with id ${game_id} moved to cell ${cell_index}`);
+        let index = active_games.findIndex((a) => a.game_id == game_id);
+
+        if (index === -1) { console.log("ERROR: GAME NOT FOUND"); return; }
+
+        active_games[index].state[cell_index] = active_games[index].currentPlayer;
+        io.emit('updateGame', active_games[index]);
+
+        if (active_games[index].checkGameOver()) {
+            archived_games.push(active_games[index]);
+            io.emit('gameOver', archived_games[archived_games.length - 1]);
+            active_games.splice(index, 1);
+            return;
+        } else {
+            active_games[index].currentPlayer = active_games[index].currentPlayer === 'X' ? 'O' : 'X';
+        }
+    });
 
     socket.on('disconnect', () => {
         console.log(`player with id ${socket.id} disconnected`);
